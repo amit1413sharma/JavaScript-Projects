b1a61bd828c95fe79932e4236dfcde345b88849f
amit1413sharma <amit1413sharma@gmail.com>
2025-08-19 11:21:37 -0400

    These are Module 1 assignments

---
+++ Basic JavaScript Projects/Project1_expressions_alert/JS/main.js
@@ -0,0 +1,20 @@
+// Declare two string variables
+var Sent1 = "This is the beginning of the string";
+var Sent2 = " and this is the end of the string";
+
+// Concatenate the two strings and store in a new variable
+var fullSentence = Sent1 + Sent2;
+
+// Display the concatenated string in a popup alert
+window.alert(fullSentence);
+
+// Write the concatenated string to the web page
+document.write(fullSentence);
+
+// Write an expression and store the result
+var result = 25 * 4; // This is a simple multiplication expression
+
+// Write the result of the expression to the web page
+document.write("<br>25 multiplied by 4 is: " + result);
+
+// Comments like these explain what the code is doing
